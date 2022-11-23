5b33f0104f9eb4b173972bfd04b4b0b09fe5071b
ATHZoA79 <106025017anthonyhsu@gmail.com>
2022-11-23 16:33:16 +0800

    1632
    - 圖片預覽
    - 測試轉換Blob

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,8 @@
         <div class="m-2 p-5">
             <label for="">1. </label>
             <input type="file" name="" accept="image/*">
-            <img src="" alt="" id="preview_1" width="200" height="200">
+            {{-- <img src="" alt="" id="preview_1" width="200" height="200"> --}}
+            <img src="" alt="" id="preview_1">
         </div>
         {{-- <div class="m-2 p-5">
       <label for="">1. </label>
@@ -32,17 +33,41 @@
         input.addEventListener('change', (e) => {
             const reader = new FileReader();
             console.log(input.files[0]);
-            
+
             reader.addEventListener("load", () => {
-              // convert image file to base64 string
-              img.src = reader.result;
+                // convert image file to base64 string
+                img.src = reader.result;
+                let testBlob = dataURItoBlob(reader.result);
+                console.log(testBlob);
             }, false);
-            
+
             if (input.files) {
-              reader.readAsDataURL(input.files[0]);
-              console.log(reader);
+                reader.readAsDataURL(input.files[0]);
+                console.log(reader);
             }
         });
+
+        function dataURItoBlob(dataURI) {
+            // convert base64/URLEncoded data component to raw binary data held in a string
+            var byteString;
+            if (dataURI.split(',')[0].indexOf('base64') >= 0)
+                byteString = atob(dataURI.split(',')[1]);
+            else
+                byteString = unescape(dataURI.split(',')[1]);
+
+            // separate out the mime component
+            var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+            // write the bytes of the string to a typed array
+            var ia = new Uint8Array(byteString.length);
+            for (var i = 0; i < byteString.length; i++) {
+                ia[i] = byteString.charCodeAt(i);
+            }
+
+            return new Blob([ia], {
+                type: mimeString
+            });
+        }
     </script>
 </body>
 
